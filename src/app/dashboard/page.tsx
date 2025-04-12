0a80a651d026689be5da7bdbec93933121eedd5c
AshGreyG <ashgrey.huaier@gmail.com>
2025-04-12 21:39:43 +0800

    :lipstick: fix the data dashboard responsive problem and the background of body element

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,14 +13,14 @@ import { cn } from "@/lib/utils";
 import "../globals.css";
 
 function Dashboard() {
-  const [supplyingValue, setSupplyingValue] = useState<string>("");
-  const [borrowingValue, setBorrowingValue] = useState<string>("");
-  const [apyValue,             setAPYValue] = useState<string>("");
+  const [supplyingValue, setSupplyingValue] = useState<string>("2.2B");
+  const [borrowingValue, setBorrowingValue] = useState<string>("2.2B");
+  const [apyValue,             setAPYValue] = useState<string>("5.75");
 
   const glassGardItems = [
-    { color: "#9b87f5", title: "Supply", icon: PiggyBank, value: supplyingValue },
-    { color: "#81c8be", title: "Borrow", icon: HandCoins, value: borrowingValue },
-    { color: "#ef9f76", title: "APY", icon: TrendingUp, value: apyValue }
+    { color: "#9b87f5", title: "Supply", icon: PiggyBank, value: supplyingValue, unit: "USDT" },
+    { color: "#81c8be", title: "Borrow", icon: HandCoins, value: borrowingValue, unit: "USDT" },
+    { color: "#ef9f76", title: "APY", icon: TrendingUp, value: apyValue, unit: "%" }
   ];
 
   // I find a safe way to dynamically generate the template string in tailwindCSS
@@ -33,20 +33,21 @@ function Dashboard() {
           "data-dashboard", // user-defined className
           "flex-1 flex flex-col px-10"
         )}>
-          <div className={`flex-1 flex flex-row justify-between`}>
+          <div className={`flex-1 flex flex-row justify-between gap-7`}>
             {glassGardItems.map((item) => (
               <GlassCard 
-                className={cn("text-center w-40", "hover:shadow-[" + item.color + "]")} 
+                className={cn("text-center flex-1", "hover:shadow-[" + item.color + "]")} 
                 hoverEffect={true}
                 key={item.title}
               >
                 <item.icon className={"h-12 w-12 mx-auto mb-2"} color={item.color} />
                 <h3 className={`text-[#99a1af] font-semibold text-xl`}>{item.title}</h3>
-                <div className={
-                  `text-wrap w-full pt-5 text-white text-md flex items-center justify-center 
-                   force-break`
-                }>
-                  {item.value}
+                <div className={`flex flex-row items-center justify-center pt-5`}>
+                  <p>
+                    <span className={`text-white text-2xl font-extrabold`}>{item.value}</span>
+                    <span>{" "}</span>
+                    <span className={`text-[#99a1af] text-xs`}>{item.unit}</span>
+                  </p>
                 </div>
               </GlassCard>
             ))}
